49368f34de2055c345a39badeb62539d53c365f4
sahithirayapati <sahithi.rayapati2025@vitstudent.ac.in>
2025-08-07 00:36:23 +0530

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include"stdio.h"
+#include"omp.h"
+//#include"anudrvprg_020_runtime_environment_routines.h"
+//#include "030_wsc_for_pp1.h"
+//#include"run_time_environment_routines.h"
+//#include "031_wsc_for_num_square.h"
+//#include "ret_practice.h"
+//#include"ret_et_helloworld.h"
+#include "mnudrvprg_030_for_worksharing_construct.h"
+//#include "mnudrvprg_010_parallel_construct.h"
+
+
+
+int main() {
+    //fn_anudrvprg_020_runtime_environment_routines();
+    //fn_wsc_for_2constructs();
+    // fn_wsc_for_1construct();
+        //fn_num_square();
+    /*fn_rter_omp_get_num_threads();
+    fn_rter_omp_set_num_threads();
+    fn_rter_omp_get_num_procs();
+    fn_rter_omp_get_max_threads();
+    fn_rter_omp_get_thread_num();
+    fn_rter_omp_in_parallel();
+    fn_rter_omp_set_dynamic();
+    fn_rter_omp_get_dynamic();
+    fn_rter_omp_set_nested();
+    fn_rter_omp_get_nested();
+   // fn_rter_omp_get_wtime();
+    //fn_rter_omp_get_wtick();
+    */
+    /*fn_rer_practice1_srl();
+    fn_rer_practice1_prl();
+    fn_rer_practice_speedup();
+    fn_rer_practice_efficiency();
+    */
+    //fn_ret_et_helloworld_srl();
+    //fn_ret_et_helloworld_par();
+    //fn_mnudrvprg_030_for_worksharing_construct();
+    //fn_mnudrvprg_010_parallel_construct();
+    fn_mnudrvprg_030_for_worksharing_construct();
+    
+        return 0;
+}
