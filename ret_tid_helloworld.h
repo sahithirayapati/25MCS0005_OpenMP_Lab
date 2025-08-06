33b24442f121b87b61a50c4c9f26f91e367b88e5
sahithirayapati <sahithi.rayapati2025@vitstudent.ac.in>
2025-08-06 23:32:33 +0530

    ret_tid_helloworld.h

---
+++ ret_tid_helloworld.h
@@ -0,0 +1,16 @@
+#include"stdio.h"
+#include"omp.h"
+
+void fn_ret_tid_helloworld_srl() {
+
+	printf("Hello World Serial printed by thread ID: %d\n", omp_get_thread_num());
+
+}
+void fn_ret_tid_helloworld_prl() {
+#pragma omp parallel
+    {
+        int tid = omp_get_thread_num();
+        printf("Hello World Parallel printed by thread ID: %d\n", tid);
+        
+    }
+}
